3c0bd68779b1df35b83e7e4cba4fa8effbdc049d
Cortega2-1 <cortega2@mail.umw.edu>
2022-04-16 09:21:54 -0400

    Final Update

--- Project5/ShoeInventory.java
+++ Project5/ShoeInventory.java
@@ -2,9 +2,10 @@ import java.io.BufferedReader;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
+import java.util.Scanner;
 
 /**
- * Write a description of class ShoeInventory here.
+ * ShoeInventory reads an input file and outputs the shoe information 
  *
  * @author Carlos Ortega
  * @version 04-15-2022
@@ -19,26 +20,38 @@ public class ShoeInventory
         FavoriteShoe.setSize(11.0);
         FavoriteShoe.setPrice(500.0);
         
+        //Check if shoe equals my size (true)
+        System.out.println(FavoriteShoe.equalSize());
+        
+        Shoes smallShoe = new Shoes("Nike", "Purple", 5.5, 65.0);
+        
+        //Check if shoe equals my size (false)
+        System.out.println(smallShoe.equalSize());
+        
         Shoes SecondShoe = new Shoes("NB", "Pink", 11.0, 175.0);
         System.out.println("Shoe Information: " + SecondShoe);
         
         //Read ShoeCollection File
         BufferedReader reader;
         try {
-            reader = new BufferedReader(new FileReader("ShoeCollection.txt"));
+            Scanner input = new Scanner(System.in);
+            System.out.println("Please enter the name of the text file: ");
+            String filename = input.nextLine();
+            reader = new BufferedReader(new FileReader(filename));
             String line;
-            while ((line = reader.readLine()) != null) {
-                // split the line by whitespaces
+            while ((line = reader.readLine()) != null) 
+            {
                 String parts[] = line.split("\\s+");
-
-                Shoes x = new Shoes(parts[0], parts[1], Double.parseDouble(parts[2]), Double.parseDouble(parts[3]));
-                System.out.println("Shoe Information: " + x);
+                Shoes newList = new Shoes(parts[0], parts[1], Double.parseDouble(parts[2]), Double.parseDouble(parts[3]));
+                System.out.println("Shoe Information: " + newList);
             }
-
             reader.close();
-        } catch (FileNotFoundException e) {
-           e.printStackTrace();
+        } 
+        catch (FileNotFoundException e) 
+        {
+           
+           System.err.println("File not found. Fix spelling errors."); 
+           System.err.println("Make sure file is created.");
         }
-
     }
 }
--- Project5/Shoes.java
+++ Project5/Shoes.java
@@ -1,6 +1,6 @@
 
 /**
- * Write a description of class Shoes here.
+ * Shoes create objects that create a brand, color, size and price.
  *
  * @author Carlos Ortega
  * @version 04-15-2022
@@ -11,7 +11,6 @@ public class Shoes
     private String color;
     private double size;
     private double price;
-    
     public Shoes()
     {
       this.brand = "";
@@ -19,6 +18,13 @@ public class Shoes
       this.color = "";
       this.price = -1;
     }
+    /**
+     * 
+     * @param brand
+     * @param color
+     * @param size
+     * @param price
+     */
     public Shoes(String brand, String color, double size, double price)
     {
       this.brand = brand;
@@ -26,38 +32,79 @@ public class Shoes
       this.size = size;
       this.price = price;
     }
+    /*
+     * @return brand
+     */
     public String getBrand()
     {
         return this.brand;
     }
-     public String getColor()
+    /*
+     * @return color
+     */
+    public String getColor()
     {
         return this.color;
     }
+    /*
+     * @return size
+     */
     public double getSize()
     {
         return this.size;
     }
+    /*
+     * @return price
+     */
     public double getPrice()
     {
         return this.price;
     }
+    /*
+     * @param brand
+     */
     public void setBrand(String b)
     {
        this.brand = b; 
     }
+    /*
+     * @param color 
+     */
     public void setColor(String c)
     {
         this.color = c;
     }
+    /*
+     * @param size
+     */
     public void setSize(double s)
     {
       this.size = s;  
     }
+    /*
+     * @param price
+     */
     public void setPrice(double p)
     {
         this.price = p;
     }
+    /*
+     * @return boolean
+     */
+    public boolean equalSize()
+    {
+        if(this.size == 11.0)
+        {
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+    }
+    /*
+     * @return shoe
+     */
     public String toString()
     {
       String s = this.brand + "," + this.size + "," + this.color + "," + this.price;
